f81de15a889f2318b9307b6baecd1bda09e45985
KevinBaoB <kevinbaobui@gmail.com>
2022-05-24 16:08:00 -0500

    Factorial

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,11 @@
-exports.factorial = function(num) {
-};
+// exports.factorial = function (num) {};
+
+function factorial(num) {
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return console.log(product);
+}
+
+factorial(5);
